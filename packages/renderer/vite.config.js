158f82d76c3a45f8dde8b5040d45a406983cc9c7
Diego Gambarotto <34460627+gambarotto@users.noreply.github.com>
2024-02-10 20:32:38 -0300

    trocado de vue para react

--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 
 import {chrome} from '../../.electron-vendors.cache.json';
-import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 import {renderer} from 'unplugin-auto-expose';
 import {join} from 'node:path';
 
@@ -42,7 +42,7 @@ const config = {
     environment: 'happy-dom',
   },
   plugins: [
-    vue(),
+    react(),
     renderer.vite({
       preloadEntry: join(PACKAGE_ROOT, '../preload/src/index.ts'),
     }),
